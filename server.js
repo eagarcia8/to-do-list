d64a894e77089a6200700abb561d4e92b164e293
Eduardo Garcia <eagarcia8@gmail.com>
2018-04-12 19:41:02 +0000

    New Files

--- server.js
+++ server.js
@@ -1,84 +1,72 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+const fs = require('fs');
+const express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
+// MongoDB Stuff
+var mongoose = require('mongoose');
+var url = 'mongodb://user:user@ds137207.mlab.com:37207/notesdb34';
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
+mongoose.connect(url, function(error) {
+    if (error){
+        console.log("Sorry something happened: " + error);
+    } else {
+        console.log("Connected to MongoDB database.")
     }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
+
+var Schema = mongoose.model('Notes', {
+    notes: String
+});
+
+// Express 'Routes'
+app.use('/client', express.static('client/'));
+app.use('/js', express.static('client/js'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+http.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started");
+});
+
+app.get('/getNotes', function (request, result) {
+    console.log("Data requested, and sent.");
+    
+    Schema.find({}, function (error, notes) {
+            if (error) {
+                console.log("Sorry we had issues searching the database: " + error)
+            } else {
+                result.send(notes);
+            }
+        });
+});
+
+app.post('/deleteNote', function (request, result) {
+    console.log(request.body);
+});
+
+app.post('/submitNote', function (request, result) {
+    console.log(request.body);
+    
+    var newNote = Schema(request.body);
+    newNote.save(function (error) {
+    if (error) {
+        console.log("Sorry the document was not saved: " + error);
+    } else {
+        console.log("Document was saved!");
+        
+        Schema.find({}, function (error, notes) {
+            if (error) {
+                console.log("Sorry we had issues searching the database: " + error)
+            } else {
+                io.emit('important', notes);
+            }
+        });
+    }
+});
+    
+    result.send(200);
+});
+
+console.log("hello");
